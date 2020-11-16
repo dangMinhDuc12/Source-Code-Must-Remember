81a9eaf7571ad9504abf3571db158a7253b14072
dangminhduc <dangminhduca3@gmail.com>
2020-11-16 20:50:14 +0700

    update Login/Register modal

---
+++ HTML-CSS-Website/app.js
@@ -0,0 +1,14 @@
+// let btn1 = document.getElementById("test");
+
+// let div1 = document.getElementById("div");
+// let div2 = document.getElementById("div2");
+// btn1.onclick = function () {
+//   div1.style.display = "none";
+//   div2.style.display = "block";
+// };
+
+// let btn2 = document.getElementById("test2");
+// btn2.onclick = function () {
+//   div2.style.display = "none";
+//   div1.style.display = "block";
+// };
