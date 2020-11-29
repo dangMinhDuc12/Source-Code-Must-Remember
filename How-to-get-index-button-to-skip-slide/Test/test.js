b6bc321ac33f5d9453869aab27204040b521d88f
dangminhduc <dangminhduca3@gmail.com>
2020-11-29 13:27:34 +0700

    complete slide show

--- How-to-get-index-button-to-skip-slide/Test/test.js
+++ How-to-get-index-button-to-skip-slide/Test/test.js
@@ -1,21 +1,25 @@
-let button = document.querySelectorAll("li");
-console.log(button);
+let button = document.querySelectorAll(".button__item");
+let slide = document.querySelectorAll(".image__item-img");
 
 for (let i = 0; i < button.length; i++) {
   button[i].onclick = function (e) {
-    for (let i = 0; i < button.length; i++) {
-      button[i].classList.remove("active");
-    }
     let buttonActive = e.target;
-    buttonActive.classList.add("active");
-
     let indexButton;
+    for (let i = 0; i < button.length; i++) {
+      button[i].classList.remove("button__active");
+    }
+    for (let i = 0; i < slide.length; i++) {
+      slide[i].classList.remove("image__item-img--active");
+    }
+
+    buttonActive.classList.add("button__active");
 
     for (
       indexButton = 0;
       (buttonActive = buttonActive.previousElementSibling);
       indexButton++
     ) {}
-    console.log(indexButton);
+
+    slide[indexButton].classList.add("image__item-img--active");
   };
 }
