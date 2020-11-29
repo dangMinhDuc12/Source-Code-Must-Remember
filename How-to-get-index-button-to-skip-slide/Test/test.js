f7f3667740acbcb7455c5255a291e484a86c8871
dangminhduc <dangminhduca3@gmail.com>
2020-11-29 18:35:42 +0700

    update option 2

--- How-to-get-index-button-to-skip-slide/Test/test.js
+++ How-to-get-index-button-to-skip-slide/Test/test.js
@@ -1,3 +1,8 @@
+/**Phải gắn cả if else vào để kiểm tra vì nếu chỉ gắn if không thì kiểm tra index và chuyển ảnh
+ * sẽ bị chậm nhau 1 nhịp dẫn đến chuyển ảnh rồi mới kiểm tra dẫn đến không chuyển được ảnh. Vì thế
+ * phải dùng if else đẻ có thể check index trước rồi mới chuyển ảnh
+ */
+
 let button = document.querySelectorAll(".button__item");
 let slide = document.querySelectorAll(".image__item-img");
 
@@ -23,13 +28,39 @@ function checkIndexButton(buttonCheck) {
 
 // Handle auto slide
 let autoslide = setInterval(function () {
+  //Option 1
+
+  // // Call button activated
+  // let buttonActive = document.querySelector(".button__active");
+
+  // // Find index button activated
+  // let indexButton = checkIndexButton(buttonActive);
+
+  // if (indexButton < button.length - 1) {
+  //   //clear class
+  //   clearAllClass();
+
+  //   //Add class to render
+
+  //   button[indexButton + 1].classList.add("button__active");
+  //   slide[indexButton + 1].classList.add("image__item-img--active");
+  // } else {
+  //   clearAllClass();
+  //   button[0].classList.add("button__active");
+  //   slide[0].classList.add("image__item-img--active");
+  // }
+
+  //Option 2
   // Call button activated
   let buttonActive = document.querySelector(".button__active");
 
   // Find index button activated
   let indexButton = checkIndexButton(buttonActive);
-
-  if (indexButton < button.length - 1) {
+  if (indexButton == button.length - 1) {
+    clearAllClass();
+    button[0].classList.add("button__active");
+    slide[0].classList.add("image__item-img--active");
+  } else {
     //clear class
     clearAllClass();
 
@@ -37,10 +68,6 @@ let autoslide = setInterval(function () {
 
     button[indexButton + 1].classList.add("button__active");
     slide[indexButton + 1].classList.add("image__item-img--active");
-  } else {
-    clearAllClass();
-    button[0].classList.add("button__active");
-    slide[0].classList.add("image__item-img--active");
   }
 }, 2000);
 
