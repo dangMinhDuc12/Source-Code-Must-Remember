2bb078aa0721adfc797a3e5a8f8795ee5e88c684
dangminhduc <dangminhduca3@gmail.com>
2020-12-13 13:53:48 +0700

    update nodejs pj

--- Study/NodeJS/Blog/src/index.js
+++ Study/NodeJS/Blog/src/index.js
@@ -12,12 +12,13 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
-
 //Call Express
 const app = express();
-
 //Init Port
 const port = 3000;
+
+//Dùng các file tĩnh (static file) như là img hay css
+app.use(express.static(path.join(__dirname, "public")));
 
 //HTTP Logger
 app.use(morgan("combined"));
@@ -40,7 +41,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/news", (req, res) => {
+  console.log(req.query.q);
   res.render("news"); //Đang dùng Express-Handlesbars
+});
+app.get("/search", (req, res) => {
+  // console.log(req.query.q);lấy ra query parameter khi người dùng nhập vào url ?q=&param2&param3
+  res.render("search");
+});
+
+// //Tạo đường dẫn(route) cho POST
+app.post("/search", (req, res) => {
+  //res.render("home"): Hiển thị file views/home ra màn hình
+  //res.send("123"):Gửi chuỗi 123 ra màn hình
+  //Query parameter là req.query còn Form Data là req.body
+
+  console.log(req.body);
+
+  res.send("");
 });
 
 //Tạo Port cho web
