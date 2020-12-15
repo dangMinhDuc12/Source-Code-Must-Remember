c3f4cb2e263079ca9a429d486638de0cfb5a3292
dangminhduc <dangminhduca3@gmail.com>
2020-12-15 13:48:28 +0700

    update mongoose collection name learned

--- Study/NodeJS/Blog/src/app/models/Course.js
+++ Study/NodeJS/Blog/src/app/models/Course.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-    name: { type: String },
-    description: { type: String },
-    image: { type: String },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: { type: String },
+        description: { type: String },
+        image: { type: String },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+    },
+    { collection: 'courses' },
+); // thêm option collection để viết tên cho collection của mình, không cho mongoose tự động thay đổi tên của collection nữa
 
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model('courses', Course);
